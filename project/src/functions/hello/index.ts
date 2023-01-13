9e99d710bd60913505f1d9781d1a4e6c1fe61eb5
Lio Dumitrescu <lio.dumitresc@gmail.com>
2023-01-13 17:51:36 +0200

    add api key

--- project/src/functions/hello/index.ts
+++ project/src/functions/hello/index.ts
@@ -8,7 +8,7 @@ export default {
       http: {
         method: 'post',
         path: 'addUserName',
-        //private: true,
+        private: true,
         request: {
           schemas: {
             'application/json': schema,
